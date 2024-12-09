0a1d3b52c733e4ae3de97fcd3afa91afe2e5a2c9
Ethan Villarosa <eacvillarosa@gmail.com>
2024-12-09 10:30:41 -0800

    fixed join friend group

--- 111project/src/pages/InterestGroups.jsx
+++ 111project/src/pages/InterestGroups.jsx
@@ -52,7 +52,7 @@ function InterestGroups() {
     };
 
     const handleJoinInterestGroup = (groupName) => {
-        fetch("http://127.0.0.1:5000/interest_group/join", {
+        fetch("http://127.0.0.1:5000/interest_groups/join", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ group_name: groupName, member_name: "Alice Jones" }),
